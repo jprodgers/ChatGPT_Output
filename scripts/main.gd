8b61b998d225d39de0248e070aed9d5dcadf6901
Jimmie Rodgers <jimmie@jimmieprodgers.com>
2025-12-17 19:39:41 -0500

    Not sure what's up.

--- scripts/main.gd
+++ scripts/main.gd
@@ -3193,25 +3193,6 @@ static func sim_job_wolfram(grid_in: PackedByteArray, grid_size_in: Vector2i, ru
 		next_row = (wolfram_row_local + 1) % grid_size_in.y
 	return {"grid": next_state, "row": next_row, "changed": changed_ref[0]}
 
-static func _sim_wolfram_element(idx: int, grid_in: PackedByteArray, grid_size_in: Vector2i, edge_mode_in: int, rule: int, source_row: int, target_row: int, next_state: PackedByteArray, changed_ref: Array, change_mutex: Mutex) -> void:
-	if idx < 0 or idx >= grid_size_in.x:
-		return
-	if target_row < 0 or target_row >= grid_size_in.y:
-		return
-	var expected: int = grid_size_in.x * grid_size_in.y
-	if expected <= 0 or next_state.size() != expected or grid_in.size() != expected:
-		return
-	var left: int = sim_job_sample_cell(grid_in, grid_size_in, edge_mode_in, idx - 1, source_row)
-	var center: int = sim_job_sample_cell(grid_in, grid_size_in, edge_mode_in, idx, source_row)
-	var right: int = sim_job_sample_cell(grid_in, grid_size_in, edge_mode_in, idx + 1, source_row)
-	var key: int = (left << 2) | (center << 1) | right
-	var state: int = (rule >> key) & 1
-	var dst: int = target_row * grid_size_in.x + idx
-	if dst < 0 or dst >= next_state.size():
-		return
-	if next_state[dst] != state:
-		_mark_sim_changed(changed_ref, change_mutex)
-	next_state[dst] = state
 
 static func _sim_wolfram_element(idx: int, grid_in: PackedByteArray, grid_size_in: Vector2i, edge_mode_in: int, rule: int, source_row: int, target_row: int, next_state: PackedByteArray, changed_ref: Array, change_mutex: Mutex) -> void:
 	if idx < 0 or idx >= grid_size_in.x:
